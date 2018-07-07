df9d3db5d8be2e42f63b6bce1cb75766026298f2
Maxim <maxrap7878@mail.ru>
2018-07-07 11:03:17 +0300

    before adding functional

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -2,18 +2,21 @@ import React, {Component} from 'react'
 import "./assets/styles/style.scss"
 import Button from '@material-ui/core/Button';
 import Input  from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox'
 
 export default class TestComp extends Component{
     render(){
        return(
            <div className="container">
-               <div className="list-body">List</div>
+               <div className="list-body">
+                   <div className="list-item"><Checkbox/><div>todo something</div></div>
+                   <div className="list-item"><Checkbox/><div>todo something</div></div>
+                   <div className="list-item"><Checkbox/><div>todo something</div></div>
+               </div>
                <div className="functional-container">
                <Input/>
                <Button className="button" variant="contained" color='secondary' >ADD</Button>
                </div>
-
-
            </div>
        )
     }
